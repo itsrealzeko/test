a960c3683fe9e049516404aed7a5ea553cbc81f2
itsrealzeko <itsrealzeko@gmail.com>
2024-12-17 00:04:13 +0300

    comment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './style.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>test</h1>
+
+    </>
+  )
+}
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+
+let root = document.getElementById('root')
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
